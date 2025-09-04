6332f0208822cb335952ebd4dae04bb6532ca578
LOLO SUPSSS <Lolsupz470@gmail.com>
2025-09-04 19:43:52 -0300

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-6 text-center bg-green-700 text-white mt-10">
+      <p>© {new Date().getFullYear()} Ecologic. Todos os direitos reservados.</p>
+    </footer>
+  );
+}
