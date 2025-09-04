db075863d08a4e1e226f5887c9bdcfb1389e1bd9
LOLO SUPSSS <Lolsupz470@gmail.com>
2025-09-04 19:43:18 -0300

    Create Impact.js

---
+++ components/Impact.js
@@ -0,0 +1,21 @@
+export default function Impact() {
+  return (
+    <section id="impact" className="py-20 px-8 max-w-5xl mx-auto text-center">
+      <h3 className="text-3xl font-bold mb-6">Nosso Impacto</h3>
+      <div className="grid md:grid-cols-3 gap-8">
+        <div>
+          <h4 className="text-4xl font-extrabold text-green-600">+50</h4>
+          <p>Projetos concluídos</p>
+        </div>
+        <div>
+          <h4 className="text-4xl font-extrabold text-green-600">+10k</h4>
+          <p>Pessoas impactadas</p>
+        </div>
+        <div>
+          <h4 className="text-4xl font-extrabold text-green-600">100%</h4>
+          <p>Compromisso sustentável</p>
+        </div>
+      </div>
+    </section>
+  );
+}
